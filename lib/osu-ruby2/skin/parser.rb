9b90e9e3d2bb604a82219115d57e1095e85b88e8
Rei Hakurei <reimu_after_marisa@yahoo.com>
2022-07-04 14:47:13 +0700

    add basic functionality to read/write (mod not yet) osu!skins.

--- lib/osu-ruby2/skin/parser.rb
+++ lib/osu-ruby2/skin/parser.rb
@@ -1,23 +1,44 @@
+# file content mostly copied from skin-reader.rb
 require 'osu-ruby2'
+require 'osu-ruby2/error'
+require 'osu-ruby2/parser'
+
+module OsuRuby::Error
+  # No Skin error.
+  class SkinNotFoundError < StandardError; end
+end
 module OsuRuby::Skin
+  # osu!skin metadata reader.
+  # @todo Fix naming convention for this.
   class Processor
-    FILE_NAME = 'skin.ini'
+    # osu!skin metadata filename to find
+    FILE_NAME = -'skin.ini'
+    
+    # @param base_dir [String, nil] Expected skin directory to check
     def initialize(base_dir)
-      @base_dir  = Dir.pwd || base_dir
+      @base_dir  = base_dir || Dir.pwd
       @skin_data = nil
     end
-    def parse
+    # Attempts to read +skin.ini+ file.
+    # @return [void]
+    def process
       skin_dir
       read_file
     end
+    # Detected osu!skin folder
+    # @!attribute [r] skin_dir
+    # @return [String] actual osu!skin directory
+    # @raise [Error::SkinNotFoundError] if +skin.ini+ is not found until root
+    #   directory.
     def skin_dir
       return @skin_dir if @skin_dir
+      # Ascends directory to check until reaches root.
       no_result = false
       @skin_dir = @base_dir
       loop do
         $stderr.puts "Scanning on #{@skin_dir}"
-        break if File.exist?(File.join(@skin_dir,FILE_NAME))
-        next_dir = File.expand_path("..",@skin_dir)
+        break if File.exist?(skin_file)
+        next_dir = File.expand_path("..", @skin_dir)
         if next_dir == @skin_dir then
           no_result = true
           break
@@ -27,14 +48,29 @@ module OsuRuby::Skin
       end
 
       if no_result then
-        fail "Cannot find skin.ini"
+        @skin_dir = nil
+        fail Error::SkinNotFoundError, "Cannot find skin.ini"
       else
         @skin_dir
       end
     end
+    # Obtain currently read skin file from +skin.ini+
+    # @!attribute [r] skin_file
+    # @return [String]
+    def skin_file
+      return unless @skin_dir
+      File.join(@skin_dir, FILE_NAME)
+    end
+    # Parse +skin.ini+ file into program readable object.
+    # @return [void]
     def read_file
-      @skin_data = OsuRuby::FileData.new(File.read(File.join(@skin_dir,FILE_NAME)))
-      @skin_data.process
+      @skin_data = OsuRuby::Parser::FileData.load(File.read(skin_file))
+      @skin_data
+    end
+    # Write skin data to respective +skin.ini+
+    # @return [void]
+    def write_file
+      @skin_data.write skin_file
     end
   end
 end
